1ae9886e5e4aca3dac42c4b8a91d66d767c9a92d
xy2374z <nicolasmeinardus@gmail.com>
2025-11-06 20:26:16 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,1 @@
-self.addEventListener("install", event => {
-  console.log("Service worker installed");
-});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
